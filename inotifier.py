5e0f290de729b90dbde73490b468cf59feab820d
Gabriele Tozzi <gabriele@tozzi.eu>
2012-03-08 06:02:24 +0100

    Can now run as daemon

--- inotifier.py
+++ inotifier.py
@@ -24,6 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
 
+import sys, os, time, atexit
+from signal import SIGTERM
 import pyinotify
 import argparse, ConfigParser
 import smtplib
@@ -33,6 +35,141 @@ import re
 
 NAME = 'iNotifier'
 VERSION = '1.1'
+
+class Daemon(object):
+    """
+    A generic daemon class.
+    
+    Usage: subclass the Daemon class and override the run() method
+    
+    @author Sander Marechal
+    @url http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+    """
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+    
+    def daemonize(self):
+        """
+        do the UNIX double-fork magic, see Stevens' "Advanced 
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+    
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+    
+        # do second fork
+        try: 
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+    
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        try:
+            file(self.pidfile,'w+').write("%s\n" % pid)
+        except IOError, e:
+            sys.stderr.write("write pid file %s failed: %d (%s)\n" % (self.pidfile, e.errno, e.strerror))
+            sys.exit(1)
+    
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+    
+    def delpid(self):
+        try:
+            os.remove(self.pidfile)
+        except OSError:
+            pass
+
+    def start(self):
+        """
+        Start the daemon
+        """
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+    
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+        
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+    
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return # not an error in a restart
+
+        # Try killing the daemon process    
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop()
+        self.start()
+
+    def run(self):
+        """
+        You should override this method when you subclass Daemon. It will be called after the process has been
+        daemonized by start() or restart().
+        """
 
 class EventHandler(pyinotify.ProcessEvent):
     ''' Handles events '''
@@ -94,7 +231,7 @@ class WatchItem():
         self.body = body
         self.btype=btype
 
-class Main():
+class Main(Daemon):
     ''' Main class '''
 
     def __init__(self):
@@ -110,9 +247,17 @@ class Main():
         parser.add_argument('-v', '--verbose', dest='verbose',
             action='store_true',
             help='also notify on stdout')
+        parser.add_argument('-f', '--foreground', dest='foreground',
+            action='store_true',
+            help='run in foreground, don\'t daemonize')
+        parser.add_argument('-p', '--pid', dest='pid',
+            default='/var/run/' + NAME.lower() + '.pid',
+            help='name of the PID file (used when daemonizing, default: %(default)s)')
         
         args = parser.parse_args()
         self.verbose = args.verbose
+        self.foreground = args.foreground
+        self.pidfile = args.pid
 
         # Read the config file
         config = ConfigParser.ConfigParser()
@@ -139,6 +284,15 @@ class Main():
                 Template(config.get(s, 'body')),
                 config.get(s, 'type'),
             )
+        
+        # Calls super init method
+        super(Main, self).__init__(self.pidfile)
+
+        # Start myself
+        if self.foreground:
+            self.run()
+        else:
+            self.start()
     
     def run(self):
         ''' Starts the daemon '''
@@ -157,4 +311,5 @@ class Main():
         notifier.loop()
 
 if __name__ == '__main__':
-    Main().run()
+    Main()
+    sys.exit(0)
